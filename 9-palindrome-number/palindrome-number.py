0eeadaa27bc3ea9f646b6f039c92cd15d7991eac
Dhruv Jindal <114129358+DhruvJDev@users.noreply.github.com>
2026-07-01 22:36:24 +0530

    Time: 11 ms (55.88%) | Memory: 12.3 MB (89.50%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,19 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        
-        # Negative numbers and numbers ending with 0
-        # (except 0 itself) cannot be palindromes
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
         reversed_half = 0
-        
-        # Reverse only half of the number
+
         while x > reversed_half:
             reversed_half = reversed_half * 10 + x % 10
             x //= 10
-        
-        # For even digits: x == reversed_half
-        # For odd digits: x == reversed_half // 10
+
         return x == reversed_half or x == reversed_half // 10
